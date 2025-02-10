7b232a8229f4d8a8cff3b785c23cf69ca3b357c2
rmaver <maver.rok@gmail.com>
2025-02-10 13:45:44 +0100

    Add initial implementation of raffle GUI with CSV parsing
    This commit introduces a basic web interface for grouping and displaying CSV data. It includes two HTML templates (`index.html` and `items.html`), a CSV file (`interna1.csv`), and the Go server logic to render these pages. Project configuration files for the development environment are also included.

--- main.go
+++ main.go
@@ -1,2 +1,48 @@
 package main
 
+import (
+	"encoding/csv"
+	"html/template"
+	"log"
+	"net/http"
+	"os"
+)
+
+func main() {
+
+	h1 := func(w http.ResponseWriter, r *http.Request) {
+		tmp1 := template.Must(template.ParseFiles("index.html"))
+		tmp1.Execute(w, nil)
+	}
+
+	internaItems := func(w http.ResponseWriter, r *http.Request) {
+
+		file, err := os.Open("interna1.csv")
+		if err != nil {
+			http.Error(w, "Could not open CSV file", http.StatusInternalServerError)
+			return
+		}
+		defer file.Close()
+
+		reader := csv.NewReader(file)
+		reader.Comma = ';'
+		records, err := reader.ReadAll()
+		if err != nil {
+			http.Error(w, "Could not read CSV file", http.StatusInternalServerError)
+			return
+		}
+
+		groupedRecords := make(map[string][][]string)
+		for _, record := range records {
+			key := record[0]
+			groupedRecords[key] = append(groupedRecords[key], record)
+		}
+
+		tmp2 := template.Must(template.ParseFiles("items.html"))
+		tmp2.Execute(w, groupedRecords)
+	}
+
+	http.HandleFunc("/", h1)
+	http.HandleFunc("/items", internaItems)
+	log.Fatal(http.ListenAndServe(":8000", nil))
+}
